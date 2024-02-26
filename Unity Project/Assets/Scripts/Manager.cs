4da0fd07a4bb540d8f2a0b10a4ee9a06c1e859d0
r Star <starlove124760@gmail.com>
2024-02-26 22:48:16 +0800

    add log

--- Unity Project/Assets/Scripts/Manager.cs
+++ Unity Project/Assets/Scripts/Manager.cs
@@ -1,14 +1,23 @@
+#region
+
 using MyNamespace;
 using UnityEngine;
+
+#endregion
 
 namespace DefulatNameSpace
 {
     public class Manager : MonoBehaviour
     {
+    #region Unity events
+
         private void Awake()
         {
             GameScore.Instance.AddScore(10);
+            Debug.Log($"{GameScore.Instance.Score}");
             // NewBehaviourScript.Instance.Test();
         }
+
+    #endregion
     }
 }
--- Unity Project/Assets/Scripts/NewBehaviourScript.cs
+++ Unity Project/Assets/Scripts/NewBehaviourScript.cs
@@ -1,11 +1,31 @@
-using AutoProperty;
+#region
+
 using UnityEngine;
 
-// [Singleton]
+#endregion
+
 namespace MyNamespace
 {
+    [Singleton]
     public partial class NewBehaviourScript : MonoBehaviour
     {
+    #region Public Variables
+
+        public Rigidbody RB
+        {
+            get
+            {
+                if (rb == null) rb = GetComponent<Rigidbody>();
+                return rb;
+            }
+        }
+
+    #endregion
+
+    #region Private Variables
+
+        private int _intValue;
+
         [AutoProp(AXS.PublicGetPrivateSet) , SerializeField]
         private float _hogehoge;
 
@@ -19,20 +39,13 @@ namespace MyNamespace
         [GetComponent(GetComponentAttribute.TargetType.Child)]
         private Rigidbody2D rigidbody2D;
 
-        private int _intValue;
-
         [SerializeField]
         // [GetComponent(GetComponentAttribute.TargetType.Child)]
         private Rigidbody rb;
 
-        public Rigidbody RB
-        {
-            get
-            {
-                if (rb == null) rb = GetComponent<Rigidbody>();
-                return rb;
-            }
-        }
+    #endregion
+
+    #region Unity events
 
         void Start()
         {
@@ -44,9 +57,15 @@ namespace MyNamespace
             // Assert.IsNotNull(RB , $"RB is null in {gameObject}");
         }
 
+    #endregion
+
+    #region Public Methods
+
         public void Test()
         {
             Debug.Log($"Test");
         }
+
+    #endregion
     }
 }
